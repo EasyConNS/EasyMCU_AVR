4b497d0ce6c7e22b134f404027c82607eefda2e2
Haocheng Liu <errolscope@gmail.com>
2020-03-01 14:37:05 -0500

    Fixed unsigned value in for-loop

--- Joystick.c
+++ Joystick.c
@@ -783,9 +783,6 @@ void Script_Task(void)
                                 {
                                     // small number
                                     FOR_C(_forstackindex - 1) = _ins & ((1 << 10) - 1);
-                                    // fill sign bit
-                                    FOR_C(_forstackindex - 1) <<= 22;
-                                    FOR_C(_forstackindex - 1) >>= 22;
                                     if (FOR_C(_forstackindex - 1) == 0)
                                     {
                                         // infinite loop
@@ -796,9 +793,6 @@ void Script_Task(void)
                                 {
                                     // large number
                                     FOR_C(_forstackindex - 1) = _insEx & ((1L << 26) - 1);
-                                    // fill sign bit
-                                    FOR_C(_forstackindex - 1) <<= 6;
-                                    FOR_C(_forstackindex - 1) >>= 6;
                                 }
                             }
                         }
